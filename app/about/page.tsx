b5c5c58d16086c67069e14c39af582a7fd32ad3a
Prad <pradyumna@aseiro.in>
2025-07-10 10:16:17 +0100

    Fixed gloablPresence error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/AboutPage/HeroSection";
 import OurStorySection from "@/components/AboutPage/OurStorySection";
 import ValuesSection from "@/components/AboutPage/ValuesSection";
 import WhatSetsUsApart from "@/components/AboutPage/WhatSetsUsApart";
-import GlobalPresence from "@/components/AboutPage/GlobalPresence";
+import GlobalPresence from "@/components/MainPage/GlobalPresence";
 import MeetTheTeam from "@/components/AboutPage/MeetTheTeam";
 import TeamStats from "@/components/AboutPage/TeamStats"; // optional
 
